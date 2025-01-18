e15eb2c9bdd70e9200fdafeaf97680eb5be3d8e5
rimuelcanada14 <102201493+rimuelcanada14@users.noreply.github.com>
2025-01-18 14:38:52 +0800

    trial

--- portfolio/src/components/SkillsPage/SkillsPage.jsx
+++ portfolio/src/components/SkillsPage/SkillsPage.jsx
@@ -19,7 +19,7 @@ const SkillsPage = () => {
           <div className="flip-front-s">
             <RxQuestionMarkCircled className="card-icon" />
             <p className="text-end fw-bold skills-title">
-              Front-end<br />Again
+              Front-end<br />Trial Again
             </p>
           </div>
           <div className="flip-back">
