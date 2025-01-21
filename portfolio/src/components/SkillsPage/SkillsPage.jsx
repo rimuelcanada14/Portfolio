20cb0347aa842f9f53cef233192716f1c8f04f19
rimuelcanada14 <102201493+rimuelcanada14@users.noreply.github.com>
2025-01-21 22:03:24 +0800

    added firebase

--- portfolio/src/components/SkillsPage/SkillsPage.jsx
+++ portfolio/src/components/SkillsPage/SkillsPage.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './SkillsPage.css';
 import { RxQuestionMarkCircled } from "react-icons/rx";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
 const SkillsPage = () => {
   const [flipped, setFlipped] = useState([false, false, false]);
@@ -11,6 +13,73 @@ const SkillsPage = () => {
     setFlipped(newFlipped);
   };
 
+  //for front-end data
+  const [Fdata, setFData] = useState([]);
+  const [Floading, setFLoading] = useState(true);
+  //for back-end data
+  const [Bdata, setBData] = useState([]);
+  const [Bloading, setBLoading] = useState(true);
+  //for back-end data
+  const [Dbdata, setDbData] = useState([]);
+  const [Dbloading, setDbLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchFData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "frontend"));
+        const data = [];
+        querySnapshot.forEach((doc) => {
+          data.push(doc.data());
+      });
+      setFData(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      } finally {
+        setFLoading(false);
+      }
+    };
+
+    fetchFData();
+  }, []);
+
+  useEffect(() => {
+    const fetchBData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "backend"));
+        const data = [];
+        querySnapshot.forEach((doc) => {
+          data.push(doc.data());
+      });
+      setBData(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      } finally {
+        setBLoading(false);
+      }
+    };
+
+    fetchBData();
+  }, []);
+
+  useEffect(() => {
+    const fetchDbData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "dbtools"));
+        const data = [];
+        querySnapshot.forEach((doc) => {
+          data.push(doc.data());
+      });
+      setDbData(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      } finally {
+        setDbLoading(false);
+      }
+    };
+
+    fetchDbData();
+  }, []);
+    
   return (
     <div className='container skills-container'>
       <p className="fw-bold skills-titles">I can do...</p>
@@ -23,7 +92,17 @@ const SkillsPage = () => {
             </p>
           </div>
           <div className="flip-back">
-            <p>This is my front-end</p>
+            {Floading ? (
+              <p>Loading...</p>
+            ) : (
+              Fdata.map((item, index) => (
+                <div key={index}>
+                  <p><strong>Name:</strong> {item.name}</p>
+                  <p><strong>Description:</strong> {item.description}</p>
+                  <p><strong>Rating:</strong> {item.rating}</p>
+                </div>
+              ))
+            )}
           </div>
         </div>
 
@@ -35,7 +114,17 @@ const SkillsPage = () => {
             </p>
           </div>
           <div className="flip-back">
-            <p>This is my back-end</p>
+          {Bloading ? (
+                <p>Loading...</p>
+            ) : (
+              Bdata.map((item, index) => (
+                <div key ={index}>
+                  <p><strong>Name:</strong> {item.name}</p>
+                  <p><strong>Description:</strong> {item.description}</p>
+                  <p><strong>Rating:</strong> {item.rating}</p>
+                </div>
+              ))
+            )}
           </div>
         </div>
 
@@ -47,7 +136,17 @@ const SkillsPage = () => {
             </p>
           </div>
           <div className="flip-back">
-            <p>This is my database and tools</p>
+          {Dbloading ? (
+                <p>Loading...</p>
+            ) : (
+              Dbdata.map((item, index) => (
+                <div key ={index}>
+                  <p><strong>Name:</strong> {item.name}</p>
+                  <p><strong>Description:</strong> {item.description}</p>
+                  <p><strong>Rating:</strong> {item.rating}</p>
+                </div>
+              ))
+            )}
           </div>
         </div>
       </div>
